Fixed "null" gradients.

Train.py
@@ -77,67 +77,13 @@ def train(model, train_dl, loss_fn, optim, opts):
         tgt = batch['tgt_input'].float().to(DEVICE)
         tgt_mask = batch['tgt_mask'].to(DEVICE)
 
-        # print(f"Source nan values: {src.isnan().sum()}")
-        # print(f"Target nan values: {tgt.isnan().sum()}")
-
-        # print("Checking for NaNs in gradients before step...")
-        # for name, param in model.named_parameters():
-        #     if param.grad is not None:
-        #         if torch.isnan(param.grad).any():
-        #             print(f"NaNs detected in gradients of {name}")
-
-        # Pass into model, get probability over the vocab out
         logits = model(src, tgt, src_mask, tgt_mask)
-
-        print("Checking for NaNs in intermediate outputs...")
-        for name, param in model.named_parameters():
-            if torch.isnan(param).any():
-                print(f"NaNs detected in {name}")
-
-        # debug_logit, debug_target = logits[0], tgt[0]
-        # print("\tTarget:")
-        # print(debug_target)
-        # print("\tOutput:")
-        # print(debug_logit)
-
-        # padding = torch.tensor([-2, -2, -2, -2])
-        # n_null, n_padding = torch.isnan(debug_logit).sum(), (debug_target == padding).sum()
-
-        # print(f"\tNull seq: {n_null}")
-        # print(f"\tPad seq: {n_padding}\n")
-
-        # print(f"Logits Shape:\t\t{logits.shape}")
-        # print(f"Target Shape:\t\t{tgt.shape}")
-        # print(f"Target Mask Shape:\t{tgt_mask.shape}")
 
         # Reset gradients before we try to compute the gradients over the loss
         optim.zero_grad()
 
-        # Get original shape back
-        # tgt_out = tgt[1:, :]
-
-        # Compute loss and gradient over that loss
-        # loss = loss_fn(torch.flatten(logits), torch.flatten(tgt))
         loss = loss_fn(logits, tgt)
-        # loss.backward()
-
-        grads = torch.autograd.grad(loss, model.parameters(), create_graph=True, retain_graph=True)
-
-        # Apply gradients, ignoring NaN values
-        n_nan = 0
-        for grad, param in zip(grads, model.parameters()):
-            if torch.isnan(grad).any():
-                n_nan += 1
-                continue
-            param.grad = grad
-
-        print(f"{n_nan}/{count_parameters(model)} parameters are NaN")
-
-        print("Checking for NaNs in gradients...")
-        for name, param in model.named_parameters():
-            if param.grad is not None:
-                if torch.isnan(param.grad).any():
-                    print(f"NaNs detected in gradients of {name}")
+        loss.backward()
 
         # Step weights
         optim.step()
@@ -299,7 +245,7 @@ if __name__ == "__main__":
                         help="Batch size")
     
     # Transformer settings
-    parser.add_argument("--attn_heads", type=int, default=2,
+    parser.add_argument("--attn_heads", type=int, default=6,
                         help="Number of attention heads")
     parser.add_argument("--enc_layers", type=int, default=2,
                         help="Number of encoder layers")

models/TransformerModel.py
@@ -29,9 +29,10 @@ class MidiTransformer(nn.Module):
             embed_size,
             num_encoder_layers=6,
             num_decoder_layers=6,
-            num_heads=2,
+            num_heads=6,
             dim_feedforward=2048,
-            dropout=0.1
+            dropout=0.1,
+            batch_first=True
         ):
         super(MidiTransformer, self).__init__()
 
@@ -47,7 +48,8 @@ class MidiTransformer(nn.Module):
             num_encoder_layers=num_encoder_layers,
             num_decoder_layers=num_decoder_layers,
             dim_feedforward=dim_feedforward,
-            dropout=dropout
+            dropout=dropout,
+            batch_first=batch_first
         )
 
         self.ff = nn.Linear(embed_size, embed_size)
@@ -67,24 +69,16 @@ class MidiTransformer(nn.Module):
         outs = self.transformer(
             src_emb,
             tgt_emb,
-            # src_mask=src_mask,
-            # tgt_mask=tgt_mask,
-            # None,
-            src_key_padding_mask=src_mask.t(),
-            tgt_key_padding_mask=tgt_mask.t()
+            src_key_padding_mask=src_mask,
+            tgt_key_padding_mask=tgt_mask
         )
 
         out = self.ff(outs)
-        print(out.shape)
 
         time = self.abs(out[...,0])
         duration = self.abs(out[...,1])
         pitch = self.softmax(out[...,2:130], dim=-1)
         velocity = self.softmax(out[...,130:258], dim=-1)
-        print(time.shape)
-        print(duration.shape)
-        print(pitch.shape)
-        print(velocity.shape)
 
         concatenated_output = torch.cat([time.unsqueeze(-1), duration.unsqueeze(-1), pitch, velocity], dim=-1)
         return concatenated_output