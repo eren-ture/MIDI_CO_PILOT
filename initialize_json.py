30a922612b176906ba5b130d2ef3cc8dcb6eb470
eren-ture <eren.ture.22@gmail.com>
2024-07-27 00:46:36 +0300

    Removed ridiculous time signatures.

--- initialize_json.py
+++ initialize_json.py
@@ -17,42 +17,46 @@ def get_song_details(dir):
     # Discard songs with only one track.
     n_tracks = sum(1 if not t.is_drum else 0 for t in midi.tracks) # Count only non-drum tracks.
     if n_tracks <= 1:
-        print(f'SINGLE TRACK: {dir}')
+        # print(f'SINGLE TRACK: {dir}')
         return None
 
     # Discard songs with multiple time signatures.
     time_signatures = midi.time_signatures
     if len(time_signatures) > 1:
-        print(f'MULTI SIGNATURE: {dir}')
+        # print(f'MULTI SIGNATURE: {dir}')
         return None
+    elif len(time_signatures) > 0:
+        t_sig = (time_signatures[0].numerator, time_signatures[0].denominator)
+        if t_sig[0] * (4 / t_sig[1]) < 1:
+            # print(f'BAR LENGTH TOO SHORT: {dir}')
+            return None
+        if t_sig[0] > 16:
+            print(f"RIDICULOUS TIME SIG: {dir}")
+            print(f"Time Signature: {t_sig}")
+            return None
 
     # Discard songs with multiple tempos.
     tempos = midi.tempos
     if len(tempos) > 1:
-        print(f'MULTI TEMPO: {dir}')
+        # print(f'MULTI TEMPO: {dir}')
         return None
-    elif len(tempos) > 0:
-        t_sig = tempos[0]
-        if t_sig[0] * (4 / t_sig[1]) < 1:
-            print(f'BAR LENGTH TOO SHORT: {dir}')
-            return None
 
     # Discard songs with multiple keys.
     keys = midi.key_signatures
     if len(keys) > 1:
-        print(f'MULTI KEY: {dir}')
+        # print(f'MULTI KEY: {dir}')
         return None
 
     song = {
         'Midi_Directory': dir,
-        'Artist': dir.split('\\')[-2],
-        'Song': dir.split('\\')[-1][:-4]
+        'Artist': dir.split(os.sep)[-2],
+        'Song': dir.split(os.sep)[-1][:-4]
     }
     return song
 
 
 ## SET VALUES ##
-data_folder = '.\\archive'
+data_folder = './archive'
 train_percentage = 0.85
 
 data = []
@@ -75,10 +79,10 @@ print(f"{train_percentage} train data")
 train_data = data[:train_cutoff]
 test_data = data[train_cutoff:]
 
-train_file_path = "data\\train_data.json"
+train_file_path = "data/train_data.json"
 with open(train_file_path, "w") as json_file:
     json.dump(train_data, json_file, indent=4)
 
-test_file_path = "data\\test_data.json"
+test_file_path = "data/test_data.json"
 with open(test_file_path, "w") as json_file:
     json.dump(test_data, json_file, indent=4)
